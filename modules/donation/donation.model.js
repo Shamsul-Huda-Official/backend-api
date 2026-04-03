d03ab0d8d6fa887902a9e35ff2ca12117878da33
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-03 20:20:27 +0530

    feat: Attendance schema pending

---
+++ modules/donation/donation.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const donationSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student',
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    month: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    institutionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Institution',
+    },
+}, { timestamps: true });
+
+const Donation = mongoose.model('Donation', donationSchema);
+module.exports = Donation;
