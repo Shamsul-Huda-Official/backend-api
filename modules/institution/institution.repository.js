587d6eff7c96b2b125ef1f3137ff2c120bf708dc
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-05 13:13:34 +0530

    feat: institution module repository file crud functionalities finished

---
+++ modules/institution/institution.repository.js
@@ -0,0 +1,44 @@
+const Institution = require('./institution.model')
+
+const createInstitution = async (data) => {
+    return await Institution.create(data)
+}
+
+const getAllInstitutions = async ({ skip = 0, limit = 10, name, search }) => {
+    const query = {};
+    if(name) {
+        query.name = name;
+    }    
+    if(search) {
+        query.name = { $regex: search, $option: 'i' };
+    }
+    const [ institution, total ] = await Promise.all([
+        Institution.find(query)
+            .skip(skip)
+            .limit(limit)
+            .sort({ createdAt: -1 })
+            .lean(),
+        Institution.countDocuments(query),
+    ])
+    return { institution, total };
+}
+
+const getInstitutionById = async (id) => {
+    return await Institution.findById(id);
+}
+
+const updateInstitution = async (id, data) => {
+    return await Institution.findByIdAndUpdate(id, data, { new: true })
+}
+
+const deleteInstitution = async (id) => {
+    return item.findByIdAndDelete(id)
+}
+
+module.exports = {
+    createInstitution,
+    getAllInstitutions,
+    getInstitutionById,
+    updateInstitution,
+    deleteInstitution,
+}
