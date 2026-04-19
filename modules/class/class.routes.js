2bd4596c869d9ebd062954b9931ccdef6ebd8c0d
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-19 21:28:57 +0530

    feat: Class routes.js work finished

--- modules/class/class.routes.js
+++ modules/class/class.routes.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const classController = require('./class.controller');
+const authMiddleware = require('../../shared/middleware/auth.middleware');
+const roleMiddleware = require('../../shared/middleware/role.middleware');
+
+router.route('/')
+    .get(classController.getAllClasses)
+    .post(classController.createClass)
+
+router.route('/:id')
+    .get(classController.getClassById)
+    .put(classController.updateClass)
+    .delete(classController.deleteClass)
+
 module.exports = router;
