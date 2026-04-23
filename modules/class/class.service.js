9ff1678163a108b2261d027724b330eb2b857bdd
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-23 11:42:37 +0530

    refactor: class.service.js migrated to postgre

--- modules/class/class.service.js
+++ modules/class/class.service.js
@@ -1,22 +1,36 @@
 const classRepository = require('./class.repository');
+const AppError = require('../../shared/errors/AppError')
+const { prisma } = require('../../shared/config/connection');
 
 const createClass = async (data) => {
     const { name, divisions, institutionId } = data;
 
     if ( !name || !institutionId ) {
-        throw new Error('Name and Institution ID are required.');
+        throw new AppError('Name and Institution ID are required.', 400);
     }
 
     if (!divisions || divisions.length === 0) {
-        throw new Error('At least one division is required.');
+        throw new AppError('At least one division is required.', 400);
     }
 
     const names = divisions.map(d => d.name);
     if (new Set(names).size !== names.length) {
-        throw new Error('Duplicate division name are not allowed.');
+        throw new AppError('Duplicate division name are not allowed.', 400);
     }
 
-    return await classRepository.createClass(data);
+    return await prisma.$transaction(async (tx) => {
+        const newClass = await classRepository.createClass({ name, institutionId }, tx);
+
+        await tx.division.createMany({
+            data: divisions.map(d => ({
+                name: d.name,
+                classId: newClass.id,
+                institutionId,
+                classTeacherId: d.classTeacherId || null,
+            }))
+        });
+        return newClass;
+    })
 }
 
 const getAllClasses = async ({ skip, limit, institutionId }) => {
@@ -25,11 +39,13 @@ const getAllClasses = async ({ skip, limit, institutionId }) => {
    if(institutionId) {
     where.institutionId = institutionId;
    }
-   return await classRepository.getAllClasses(where, skip, limit);
+   return await classRepository.getAllClasses({ where, skip, limit });
 }
 
 const getClassById = async (id) => {
-    return await classRepository.getClassById(id);
+    const data = await classRepository.getClassById(id);
+    if (!data) throw new AppError('Class not found', 404);
+    return data;
 }
 
 const updateClass = async (id, data) => {
