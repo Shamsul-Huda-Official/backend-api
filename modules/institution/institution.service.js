3f785ffbf8506df5f349b3cdce3e14711f27666f
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-23 10:29:18 +0530

    refactor: postgre connection errors fixed

--- modules/institution/institution.service.js
+++ modules/institution/institution.service.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 
 const institutionRepository = require('./institution.repository')
 const authRepository = require('../auth/auth.repository')
--- shared/config/connection.js
+++ shared/config/connection.js
@@ -1,7 +1,9 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { PrismaClient } = require("../../generated/prisma");
 const logger = require("../logger/logger");
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg(process.env.DATABASE_URL);
+const prisma = new PrismaClient({ adapter });
 
 const connectDB = async () => {
     await prisma.$connect()
