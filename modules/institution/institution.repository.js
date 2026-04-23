3c1032b890bbd6d47ae7c1a242fb1b2e335fd6ff
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-23 10:38:23 +0530

    refactor: institution.repository.js migrated to postgre

--- modules/institution/institution.repository.js
+++ modules/institution/institution.repository.js
@@ -1,38 +1,33 @@
 const Institution = require('./institution.model')
+const { prisma } = require('../../shared/config/connection');
 
-const createInstitution = async (data, session) => {
-    return await Institution.create([data], { session });
+const createInstitution = async (data, tx = prisma) => {
+    return await tx.institution.create({ data });
 }
 
 const getAllInstitutions = async ({ skip = 0, limit = 10, name, search }) => {
-    const query = {};
-    if(name) {
-        query.name = name;
-    }    
-    if(search) {
-        query.name = { $regex: search, $option: 'i' };
-    }
+    const where = {};
+    if (name) where.name = name;
+    if (search) where.name = { contains: search, mode: 'insensitive' };
     const [ institution, total ] = await Promise.all([
-        Institution.find(query)
-            .skip(skip)
-            .limit(limit)
-            .sort({ createdAt: -1 })
-            .lean(),
-        Institution.countDocuments(query),
-    ])
+        prisma.institution.findMany({
+            where, skip, take: limit, orderBy: { createdAt: 'desc'}
+        }),
+        prisma.institution.count({ where })
+    ]);
     return { institution, total };
 }
 
 const getInstitutionById = async (id) => {
-    return await Institution.findById(id);
+    return await prisma.institution.findUnique({ where: { id }});
 }
 
 const updateInstitution = async (id, data) => {
-    return await Institution.findByIdAndUpdate(id, data, { new: true })
+    return await prisma.institution.update({ where: { id}, data });
 }
 
 const deleteInstitution = async (id) => {
-    return item.findByIdAndDelete(id)
+    return await prisma.institution.delete({ where: { id }});
 }
 
 module.exports = {
