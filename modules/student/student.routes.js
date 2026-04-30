5c4396e8355112f6196db4c9d728d1c3325270d7
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-30 15:11:57 +0530

    feat: student.routes layer completed

--- modules/student/student.routes.js
+++ modules/student/student.routes.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {
+    createStudent,
+    bulkCreateStudent,
+    getAllStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent
+} = require('./student.controller')
+
+const authMiddleware = require('../../shared/middleware/auth.middleware');
+const roleMiddleware = require('../../shared/middleware/role.middleware');
+
+router.route('/')
+    .get(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getAllStudents)
+    .post(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), createStudent)
+
+router.post('/bulk', roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), bulkCreateStudent);
+
+router.route('/:id')
+    .get(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getStudentById)
+    .put(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), updateStudent)
+    .delete(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), deleteStudent)
 
 module.exports = router;
