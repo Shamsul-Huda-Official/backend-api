f949931519def3097c4f189ea5d868aeaad7499b
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-01 16:20:03 +0530

    feat: period.routes completed

--- modules/period/period.routes.js
+++ modules/period/period.routes.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const {
+    createPeriod,
+    assignSubject,
+    getAllPeriods,
+    getPeriodById,
+    deletePeriod
+} = require('./period.controller')
+
+const authMiddleware = require('../../shared/middleware/auth.middleware');
+const roleMiddleware = require('../../shared/middleware/role.middleware');
+
+router.use(authMiddleware);
+
+router.route('/')
+    .get(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getAllPeriods)
+    .post(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), createPeriod)
+
+router.route('/:id')
+    .get(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getPeriodById)
+    .put(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), assignSubject)
+    .delete(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), deletePeriod)
 
 module.exports = router;
