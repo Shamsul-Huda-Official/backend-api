a08b0ed1d0f0a427a10b2f767135eeb726ef116a
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-01 15:17:20 +0530

    feat: period.repository finished

---
+++ modules/period/period.repository.js
@@ -0,0 +1,54 @@
+const { prisma } = require('../../shared/config/connection');
+
+const createPeriods = async (data) => {
+    return await prisma.period.create({ data });
+}
+
+const assignSubject = async (id, institutionId, data) => {
+    return await prisma.period.update({
+        where: {
+            id, 
+            institution,
+        },
+        data: {
+            subjectId: data.subjectId,
+            teacherId: data.teacherId || null
+        }
+    })
+}
+
+const getAllPeriods = async ({ institutionId, classId, divisionId }) => {
+    return await prisma.period.findMany({
+        where: {
+            institutionId,
+            classId,
+            divisionId,
+        }
+    });
+};
+
+const getPeriodById = async (id, institutionId) => {
+    return await prisma.period.findUnique({
+        where: {
+            id,
+            institutionId
+        }
+    });
+};
+
+const deletePeriod = async (id, institutionId) => {
+    return await prisma.period.delete({
+        where: {
+            id,
+            institutionId
+        }
+    })
+}
+
+module.exports = { 
+    createPeriods,
+    assignSubject,
+    getAllPeriods,
+    getPeriodById,
+    deletePeriod
+}
