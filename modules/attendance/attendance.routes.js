6b972bb205af3f5912c5365b2debfee33636e738
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-02 17:26:10 +0530

    feat: attendance.routes completed

--- modules/attendance/attendance.routes.js
+++ modules/attendance/attendance.routes.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {
+    markAttendance,
+    getAttendance,
+    getAbsentPeriods,
+    recoverAttendance,
+    recoverAllAttendance
+} = require('./attendance.controller')
+
+const authMiddleware = require('../../shared/middleware/auth.middleware');
+const roleMiddleware = require('../../shared/middleware/role.middleware');
+
+router.use(authMiddleware);
+
+router.route('/')
+    .get(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getAttendance)
+    .post(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), markAttendance)
+
+router.get('/absent/:studentId', roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), getAbsentPeriods);
+router.put('/recover-all/:studentId', roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), recoverAllAttendance);
+
+router.route('/:id')
+    .put(roleMiddleware('SUPER_ADMIN', 'ADMIN', 'TEACHER'), recoverAllAttendance)
 
 module.exports = router;
