dfb8c79f39a04486e2eaee560d75ad8dc5938337
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-21 11:20:12 +0530

    prisma client import issue fuxing

--- shared/config/connection.js
+++ shared/config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { PrismaPg } = require('@prisma/adapter-pg');
-const { PrismaClient } = require("../../generated/prisma");
+const { PrismaClient } = require("@prisma/client");
 const logger = require("../logger/logger");
 
 const adapter = new PrismaPg(process.env.DATABASE_URL);
