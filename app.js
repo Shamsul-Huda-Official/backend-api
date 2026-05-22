38a0fcc2b11bf93b7759160f4fc4bb125fb8d927
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-22 14:47:36 +0530

    feat: prepariing to fetch with frontend

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const errorMiddleware = require('./shared/middleware/error.middleware');
 
 app.use(cors())
 
-app.use(express.json());
+app.use(express.json({
+    origin: '*',
+    credentials: true,
+}));
 
 app.use('/api/v1', routes);
 
